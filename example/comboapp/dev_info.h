e646412171c6c2b0884140b17bb7ad5e6acf056c
jiangji <jj19931006@163.com>
2018-10-09 11:25:05 +0800

    Add ble for wifi connect to ap

---
+++ example/comboapp/dev_info.h
@@ -0,0 +1,41 @@
+#ifndef _DEV_INFO_H_
+#define _DEV_INFO_H_
+
+/* for demo only, subject to change to user's specific ones. */
+
+/* Android demo */
+#if 0
+#define PRODUCT_ID     213226
+#define DEVICE_SECRET  "cNwnA4W7amnkgG6s8zGXSJD3nI1c7kO1"
+#define DEVICE_NAME    "112233445566"
+#define PRODUCT_KEY    "b1XVhqfan1X"
+#define PRODUCT_SECRET "iX6XqAjaCTXBv4h3"
+#endif
+
+#if 0
+#define PRODUCT_ID     479910
+#define DEVICE_SECRET  "ZsHgnJAhW9Ei2ySwui4llMi5J2NgpvZx"
+#define DEVICE_NAME    "test_chip_bk7231u_01"
+#define PRODUCT_KEY    "a1QqTObfDEZ"
+#define PRODUCT_SECRET "AvpC4arMQvSUHdTy"
+#endif
+
+#if 1
+#define PRODUCT_ID     340992
+#define DEVICE_SECRET  "CwhdmorFJ0qURTU3IJrTd5bnTJ3uQ29i"
+#define DEVICE_NAME    "test_beken_7231u_04"
+#define PRODUCT_KEY    "a1IS72XMcQT"
+#define PRODUCT_SECRET "DSQ5I7iMbIfBgzHM"
+#endif
+
+/* ios demo */
+#if 0
+#define PRODUCT_ID     577245
+#define DEVICE_SECRET  "W4UaIZDWmfmTKgI0rU1YpIJko5dV2mGJ"
+#define DEVICE_NAME    "xGvGuHkbk2845Ta3vZUy"
+#define PRODUCT_KEY    "a1qWr3Wk3Oo"
+#define PRODUCT_SECRET "oFTYrP7idjkYEBL9"
+#endif
+
+
+#endif
